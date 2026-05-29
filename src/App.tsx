b9afd6c727fc1efe359d5530b70f363926b6031a
matt silverman <silverman.me@gmail.com>
2026-05-29 16:38:49 -0700

    style: add inset border frame to match Flock/Grid aesthetic

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import { TabBar } from './components/TabBar'
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh', backgroundColor: '#101010', position: 'relative', overflow: 'hidden' }}>
+    <div style={{
+      width: '100vw', height: '100vh',
+      backgroundColor: '#101010',
+      position: 'relative', overflow: 'hidden',
+      boxShadow: 'inset 0 0 0 1px rgba(255,255,255,0.06)',
+      borderRadius: 10,
+    }}>
       <div style={{ position: 'absolute', bottom: 80, left: '50%', transform: 'translateX(-50%)' }}>
         <TabBar />
       </div>
