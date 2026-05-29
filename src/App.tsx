441078fcadd7f219a75ed6def40e498604c34470
matt silverman <silverman.me@gmail.com>
2026-05-29 12:48:38 -0700

    chore: update background color to #101010

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { TabBar } from './components/TabBar'
 function App() {
   return (
     <div style={{ minHeight: '100vh', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <div style={{ position: 'relative', width: 1000, height: 1000, backgroundColor: '#000000' }}>
+      <div style={{ position: 'relative', width: 1000, height: 1000, backgroundColor: '#101010' }}>
         <div style={{ position: 'absolute', bottom: 120, left: '50%', transform: 'translateX(-50%)' }}>
           <TabBar />
         </div>
